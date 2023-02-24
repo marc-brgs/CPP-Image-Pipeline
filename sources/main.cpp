91a7880173b8aa3ed5ce61f8ccdc4c71510e8496
Marc Brgs <66321703+marc-brgs@users.noreply.github.com>
2023-02-24 11:31:48 +0100

    Add more possible actions
    - Code optimization

--- sources/main.cpp
+++ sources/main.cpp
@@ -26,13 +26,20 @@ Mat saturate(Mat img, int dec);
 Mat horizontalFlip(Mat img);
 Mat verticalFlip(Mat img);
 Mat rotate90(Mat img);
+Mat crop(Mat img, int top, int left, int bottom, int right);
 Mat add(Mat img1, Mat img2);
 Mat product(Mat img1, Mat img2);
 
-int vcap(int value);
+uchar vcap(int value);
+void checkLength(vector<string> action, int size);
+Mat getImage(string name);
+
+
+// Global variables declaration
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+map<string, Mat> stored_images;
 
 int main(int argc, char** argv) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     // Colors test
     /*for(int k = 0; k < 255; k++) {
@@ -40,7 +47,7 @@ int main(int argc, char** argv) {
         cout << k << " test" << endl;
     }*/
 
-     // Program init and locate steps file
+    // Program init and locate steps file
     printHeader();
 
     printLine("Program start");
@@ -55,12 +62,12 @@ int main(int argc, char** argv) {
 
     // Read lines
     string line;
-	vector<vector<string>> ope_buffer;
+	vector<vector<string>> lines_comp;
 	int load_count = 0;
 
-     SetConsoleTextAttribute(hConsole, 8);
+    SetConsoleTextAttribute(hConsole, 8);
     while(getline(file, line)) {
-		vector<string> line_buffer;
+		vector<string> words_comp;
 		string delimiter = " ";
 
 		size_t pos = 0;
@@ -68,29 +75,26 @@ int main(int argc, char** argv) {
         cout << line << endl;
 		while((pos = line.find(delimiter)) != string::npos) {
 			token = line.substr(0, pos);
-			line_buffer.push_back(token);
+			words_comp.push_back(token);
 			line.erase(0, pos + delimiter.length());
 		}
-        line_buffer.push_back(line); // last token of the line
+        words_comp.push_back(line); // last token of the line
 
-		if(line_buffer.at(0) == "load")
-            load_count++;
-        
-		ope_buffer.push_back(line_buffer);
+		if(words_comp.at(0) == "load") load_count++;
+
+		lines_comp.push_back(words_comp);
     }
 
     file.close();
 
-	int operation_count = ope_buffer.size() - load_count;
-    stringstream sstm;
-    sstm << ope_buffer.size() << " lines (" << load_count << " load, " << operation_count << " operations)";
-    printSubLine(sstm.str());
+	int lines_count = static_cast<int>(lines_comp.size());
+	int operation_count = lines_comp.size() - load_count;
+    printSubLine(to_string(lines_count) +" lines ("+ to_string(load_count) +" load, "+ to_string(operation_count) +" operations)");
 
 	// Processing steps
-	map<string, Mat> stored_images;
 
-	for(int i = 0; i < ope_buffer.size(); i++) {
-		vector<string> action = ope_buffer.at(i);
+	for(int i = 0; i < lines_count; i++) {
+		vector<string> action = lines_comp.at(i);
 		string action_id = action.at(0);
 
 		string line = "";
@@ -98,16 +102,14 @@ int main(int argc, char** argv) {
 			line += action.at(k) + " ";
 		}
         stringstream sstm2;
-        sstm2 << "[" << i+1 << "/" << ope_buffer.size() << "] - " << line;
+        sstm2 << "[" << i+1 << "/" << lines_count << "] - " << line;
 		printLine(sstm2.str());
 
-        if(action_id == "load") {
-            if(action.size() != 4) { // load image.jpg as img1
-                printSubLineError("[!] Error : Load command should have 4 components");
-                return 0;
-            }
+        if(action_id == "load") { // ex: load image.png as img1
+            checkLength(action, 4);
+
             string path = action.at(1);
-            string variable_name = action.at(3);
+            string image_name = action.at(3);
             printSubLine("Loading " + path);
 
             Mat img = imread(path, IMREAD_COLOR);
@@ -115,56 +117,167 @@ int main(int argc, char** argv) {
                 printSubLineError("[!] Error : Could not read the image");
                 return 0;
             }
-            stored_images.insert({variable_name, img});
+            stored_images.insert({image_name, img});
         }
-        else if (action_id == "save") {
-            if(action.size() != 4) {
-                printSubLineError("[!] Error : Brightness command should have 4 components");
-                return 0;
-            }
-            string variable_name = action.at(1);
+        else if (action_id == "save") { // ex: save img1 as result.png
+            checkLength(action, 4);
+
+            string image_name = action.at(1);
             string file_name = action.at(3);
-            auto it = stored_images.find(variable_name);
+            auto it = stored_images.find(image_name);
 
             if (it == stored_images.end()) {
-                printSubLineError("[!] Error : Image name "+ variable_name +" not declared");
+                printSubLineError("[!] Error : Image name "+ image_name +" not declared");
                 return 0;
             }
 
-            stringstream sstm3;
-            sstm3 << "Saving " << variable_name << " as " << file_name;
-            printSubLine(sstm3.str());
+            printSubLine("Saving "+ image_name +" as "+ file_name);
             imwrite(file_name, it->second);
         }
-        else if(action_id == "brightness") {
-            if(action.size() != 3) {
-					printSubLineError("[!] Error : Brightness command should have 3 components");
-					return 0;
-				}
-				string variable_name = action.at(1);
-				int dec = stoi(action.at(2));
-				auto it = stored_images.find(variable_name);
+        else if(action_id == "brightness") { // ex: brightness img1 40
+            checkLength(action, 3);
 
-				if (it == stored_images.end()) {
-					printSubLineError("[!] Error : Image name "+ variable_name +" not declared");
-					return 0;
-				}
+			string image_name = action.at(1);
+			int dec = stoi(action.at(2));
+			Mat img = getImage(image_name);
 
-				it->second = brightness(it->second, dec);
-                stringstream sstm3;
-                sstm3 << "Step " << (i+1) << "/" << ope_buffer.size() << " - " << variable_name;
-				imshow(sstm3.str(), it->second);
-				waitKey(0);
+			img = brightness(img, dec);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
         }
+		else if(action_id == "invert") { // ex: invert img1
+            checkLength(action, 2);
+
+			string image_name = action.at(1);
+			Mat img = getImage(image_name);
+
+			img = invert(img);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
+        }
+		else if(action_id == "contrast") { // ex: contrast img1 40
+            checkLength(action, 3);
+
+			string image_name = action.at(1);
+			int dec = stoi(action.at(2));
+			Mat img = getImage(image_name);
+
+			img = contrast(img, dec);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
+        }
+		else if(action_id == "saturate") { // ex: saturate img1 40
+			checkLength(action, 3);
+
+			string image_name = action.at(1);
+			int dec = stoi(action.at(2));
+			Mat img = getImage(image_name);
+
+			img = saturate(img, dec);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
+		}
+		else if(action_id == "horizontal_flip") { // ex: horizontal_flip img1
+			checkLength(action, 2);
+
+			string image_name = action.at(1);
+			Mat img = getImage(image_name);
+
+			img = horizontalFlip(img);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
+		}
+		else if(action_id == "vertical_flip") { // ex: vertical_flip img1
+			checkLength(action, 2);
+
+			string image_name = action.at(1);
+			Mat img = getImage(image_name);
+
+			img = verticalFlip(img);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
+		}
+		else if(action_id == "rotate_90") { // ex: rotate_90 img1
+			checkLength(action, 2);
+
+			string image_name = action.at(1);
+			Mat img = getImage(image_name);
+
+			img = rotate90(img);
+
+            stringstream sstm3;
+            sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+            imshow(sstm3.str(), img);
+            waitKey(0);
+            destroyAllWindows();
+		}
         else {
             printSubLineError("[!] Error : Unknown action in pipeline-steps.txt");
-				return 0;
+			return 0;
         }
+
+		// Display image (at each operation on image)
+		if(action_id == "load" || action_id == "save") continue;
+
+		/*stringstream sstm3;
+        sstm3 << "Step " << (i+1) << "/" << lines_count << " - " << action.at(1);
+		imshow(sstm3.str(), img);
+		waitKey(0);
+		destroyAllWindows();*/
 	}
 
     printLine("Program end");
     cout << endl;
     return 0;
+}
+
+/**
+ * Throw an error and end program if tokens number of action is different of size
+ */
+void checkLength(vector<string> action, int size) {
+	if(action.size() != size) {
+		printSubLineError("[!] Error : "+ action.at(0) +" command should have "+ to_string(size) +" components");
+		exit(0);
+	}
+}
+
+/**
+ * Return image object corresponding to variable name declared in steps file
+ * Throw an error and end program if name unknown to program
+ */
+Mat getImage(string name) {
+	auto it = stored_images.find(name);
+
+	if (it == stored_images.end()) {
+		printSubLineError("[!] Error : Image name "+ name +" not declared");
+		exit(0);
+	}
+
+	return it->second;
 }
 
 void printHeader() {
@@ -206,10 +319,11 @@ void printSubLineError(string text) {
 /**
  * Cap given value between 0 and 255
  */
-int vcap(int value) {
-    if(value > 255) return 255;
-    if(value < 0) return 0;
-    return value;
+uchar vcap(int value) {
+    //if(value > 255) return 255;
+    //if(value < 0) return 0;
+    //return value;
+	return saturate_cast<uchar>(value);
 }
 
 /**
@@ -310,7 +424,7 @@ Mat horizontalFlip(Mat img) {
 
 	for(int y = 0; y < img.rows; y++) {
 		for(int x = 0; x < img.cols; x++) {
-			imgCopy.at<Vec3b>(Point(img.cols-1-x, y)) = img.at<Vec3b>(y,x); // get pixel
+			imgCopy.at<Vec3b>(Point(img.cols-1-x, y)) = img.at<Vec3b>(y,x); // set pixel
 		}
 	}
 
@@ -325,7 +439,7 @@ Mat verticalFlip(Mat img) {
 
 	for(int y = 0; y < img.rows; y++) {
 		for(int x = 0; x < img.cols; x++) {
-			imgCopy.at<Vec3b>(Point(x, img.rows-1-y)) = img.at<Vec3b>(y,x); // get pixel
+			imgCopy.at<Vec3b>(Point(x, img.rows-1-y)) = img.at<Vec3b>(y,x); // set pixel
 		}
 	}
 
@@ -340,7 +454,7 @@ Mat rotate90(Mat img) {
 
 	for(int y = 0; y < img.rows; y++) {
 		for(int x = 0; x < img.cols; x++) {
-			imgCopy.at<Vec3b>(Point(img.rows-y-1, x)) = img.at<Vec3b>(y,x); // get pixel
+            imgCopy.at<Vec3b>(Point(img.rows-y-1, x)) = img.at<Vec3b>(y,x); // set pixel
 		}
 	}
 
@@ -397,18 +511,15 @@ Mat resize(Mat img, int *dims) {
  */
 Mat* normalize(Mat *images) {
 	/*int minW = 10000, maxW = 0, minH = 10000, maxH = 0;
-
 	for(int i = 0; i < sizeof(images); i++) {
 		if(images[i].cols < minW) minW = images[i].cols;
 		if(images[i].cols > maxW) maxW = images[i].cols;
 		if(images[i].rows < minH) minH = images[i].rows;
 		if(images[i].rows > maxH) maxH = images[i].rows;
 	}
-
 	if(minW == maxW && minH == maxH) {
 		return images;
 	}
-
 	for(int i = 0; i < sizeof(images); i++) {
 		images[i] = crop(images[i], 0, 0, minW, minH);
 	}*/
@@ -554,9 +665,9 @@ Mat weightedMerge(Mat img1, Mat img2, Mat mask) {
 
 			Vec3b & color = img.at<Vec3b>(y,x);
 
-			color[0] = static_cast<int>(a_color[0] * (mask.at<uchar>(y,x)/255) + b_color[0] * (1-mask.at<uchar>(y,x)/255));
-			color[1] = static_cast<int>(a_color[1] * (mask.at<uchar>(y,x)/255) + b_color[1] * (1-mask.at<uchar>(y,x)/255));
-			color[2] = static_cast<int>(a_color[2] * (mask.at<uchar>(y,x)/255) + b_color[2] * (1-mask.at<uchar>(y,x)/255));
+			color[0] = vcap(a_color[0] * (mask.at<uchar>(y,x)/255) + b_color[0] * (1-mask.at<uchar>(y,x)/255));
+			color[1] = vcap(a_color[1] * (mask.at<uchar>(y,x)/255) + b_color[1] * (1-mask.at<uchar>(y,x)/255));
+			color[2] = vcap(a_color[2] * (mask.at<uchar>(y,x)/255) + b_color[2] * (1-mask.at<uchar>(y,x)/255));
 			img.at<Vec3b>(Point(x,y)) = color; // set pixel
         }
     }
@@ -582,9 +693,9 @@ Mat screen(Mat img1, Mat img2) {
 			Vec3b & b_color = b.at<Vec3b>(y,x); // get pixel
 
 			Vec3b & color = img.at<Vec3b>(y,x);
-			color[0] = static_cast<int>(255 - (255-a_color[0]) * (255-b_color[0]) /255);
-			color[1] = static_cast<int>(255 - (255-a_color[1]) * (255-b_color[1]) /255);
-			color[2] = static_cast<int>(255 - (255-a_color[2]) * (255-b_color[2]) /255);
+			color[0] = vcap(255 - (255-a_color[0]) * (255-b_color[0]) /255);
+			color[1] = vcap(255 - (255-a_color[1]) * (255-b_color[1]) /255);
+			color[2] = vcap(255 - (255-a_color[2]) * (255-b_color[2]) /255);
 			img.at<Vec3b>(Point(x,y)) = color; // set pixel
         }
     }
@@ -600,19 +711,15 @@ Mat screen(Mat img1, Mat img2) {
     images[0] = img1;
     images[1] = img2;
 	images = normalize(images);
-
 	Mat a = images[0], b = images[1];
 	Mat img(a.cols, a.rows, CV_8UC3, Scalar(255, 255, 255));
 	Mat screen = screen(a, b);
-
 	for(int y = 0; y < img.rows; y++) {
 		for(int x = 0; x < img.cols; x++) {
 			Vec3b & a_color = a.at<Vec3b>(y,x); // get pixel
 			Vec3b & b_color = b.at<Vec3b>(y,x); // get pixel
 			Vec3b & screen_color = b.at<Vec3b>(y,x); // get pixel
-
 			Vec3b & color = img.at<Vec3b>(y,x);
-
 			int avg = (a_color[0] + a_color[1] + a_color[2])/3;
 			if(avg < 128) { // product
 				color[0] = static_cast<int>(a_color[0] * b_color[0] /255);
@@ -623,9 +730,101 @@ Mat screen(Mat img1, Mat img2) {
 			else { // screen
 				img.at<Vec3b>(Point(x,y)) = screen_color; // set pixel
 			}
-
         }
     }
-
 	return img;
 }*/
+
+Mat blur(Mat img, int radius) {
+	Mat imgCopy = img.clone();
+
+	for(int y = 0; y < img.rows; y++) {
+		for(int x = 0; x < img.cols; x++) {
+			int sum_values[3] = {0, 0, 0};
+			int num_values = 0;
+			for(int i = -radius; i < radius+1; i++) {
+				for(int j = -radius; j < radius+1; j++) {
+					if(x+i >= 0 && x+i < img.cols && y+j >= 0 && y+j < img.rows) {
+						Vec3b & color = img.at<Vec3b>(y,x);
+						for(int c = 0; c < 3; c++)
+							sum_values[c] += color[c];
+						num_values++;
+					}
+				}
+			}
+			for(int c = 0; c < 3; c++)
+				sum_values[c] /= num_values;
+
+			Vec3b final(sum_values[0], sum_values[1], sum_values[2]);
+			imgCopy.at<Vec3b>(Point(x,y)) = final; // set pixel
+		}
+	}
+
+	return imgCopy;
+}
+
+Mat erode(Mat img, int radius) {
+	Mat imgCopy = img.clone();
+
+	for(int y = 0; y < img.rows; y++) {
+		for(int x = 0; x < img.cols; x++) {
+			Vec3b min_val(255, 255, 255);
+			for(int i = -radius; i < radius+1; i++) {
+				for(int j = -radius; j < radius+1; j++) {
+					if(x+i >= 0 && x+i < img.cols && y+j >= 0 && y+j < img.rows) {
+						Vec3b & color = img.at<Vec3b>(y,x);
+						for(int c = 0; c < 3; c++)
+							min_val[c] = min_val[c] < color[c] ? min_val[c] : color[c];
+					}
+				}
+			}
+			imgCopy.at<Vec3b>(Point(x,y)) = min_val; // set pixel
+		}
+	}
+
+	return imgCopy;
+}
+
+Mat dilate(Mat img, int radius) {
+	Mat imgCopy = img.clone();
+
+	for(int y = 0; y < img.rows; y++) {
+		for(int x = 0; x < img.cols; x++) {
+			Vec3b max_val(0, 0, 0);
+			for(int i = -radius; i < radius+1; i++) {
+				for(int j = -radius; j < radius+1; j++) {
+					if(x+i >= 0 && x+i < img.cols && y+j >= 0 && y+j < img.rows) {
+						Vec3b & color = img.at<Vec3b>(y,x);
+						for(int c = 0; c < 3; c++)
+							max_val[c] = max_val[c] > color[c] ? max_val[c] : color[c];			    		 		   			  
+					}
+				}
+			}
+			imgCopy.at<Vec3b>(Point(x,y)) = max_val; // set pixel
+		}
+	}
+
+	return imgCopy;
+}
+
+Mat rotate(Mat img, int angle, int *center) {
+	Mat imgCopy = img.clone();
+
+	float ct = cos(-angle);
+	float st = sin(-angle);
+	for(int y = 0; y < img.rows; y++) {
+		for(int x = 0; x < img.cols; x++) {
+			int a = center[0]+static_cast<int>(ct*(x-center[0])-st*(y-center[1]));
+			int b = center[0]+static_cast<int>(st*(x-center[0])+ct*(y-center[1]));
+			if(a >= 0 && a < img.cols && b >= 0 && b < img.rows) {
+				imgCopy.at<Vec3b>(Point(x,y)) = img.at<Vec3b>(b,a); // set pixel
+			}
+			else {
+				Vec3b zero(0, 0, 0);
+				imgCopy.at<Vec3b>(Point(x,y)) = zero; // set pixel
+			}
+		}
+	}
+
+	return imgCopy;
+}
